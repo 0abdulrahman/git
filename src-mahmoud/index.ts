b34ab01c17cae7599ead18f5ac3e71c4efbb0f6e
Mahmoud Kamel <kamelmahmoud335@gmail.com>
2025-09-28 21:56:08 +0300

    fix(user): add async/await, space between () and {

--- src-mahmoud/index.ts
+++ src-mahmoud/index.ts
@@ -1,5 +1,5 @@
-function getFetchUsers(){
-       return fetch("./json-placeholders.json")
+async function getFetchUsers() {
+       return await fetch("./json-placeholders.json")
        .then(res => res.json())
        .then(data => data)
        .catch(err => console.log(err))
